eef16ff8573e8234441c5bf1dd2838da580e6ab2
MylesScholz <myles.scholz@gmail.com>
2023-02-16 16:34:23 -0800

    I/O file and bckgrnd arg parsing.

--- driver.c
+++ driver.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define FILE_NAME_MAX 255
+
 struct command {
 	char* cmd;
 	int argc;
@@ -51,6 +53,27 @@ struct command* get_cmd() {
 		token = strtok_r(NULL, " ", &saveptr);
 	}
 
+	for (int i = 0; i < cmd->argc - 1; i++) {
+		char* arg = cmd->argv[i];
+		if (cmd->i_file == NULL && strcmp(arg, "<") == 0) {
+			int file_len = strlen(cmd->argv[i + 1]);
+			if (file_len > FILE_NAME_MAX) file_len = FILE_NAME_MAX;
+
+			cmd->i_file = (char*) malloc(sizeof(char) * (file_len + 1));
+			strncpy(cmd->i_file, cmd->argv[i + 1], file_len + 1);
+		} else if (cmd->o_file == NULL && strcmp(arg, ">") == 0) {
+			int file_len = strlen(cmd->argv[i + 1]);
+			if (file_len > FILE_NAME_MAX) file_len = FILE_NAME_MAX;
+
+			cmd->o_file = (char*) malloc(sizeof(char) * (file_len + 1));
+			strncpy(cmd->o_file, cmd->argv[i + 1], file_len + 1);
+		}
+	}
+
+	if (cmd->argc >= 1 && strcmp(cmd->argv[cmd->argc - 1], "&") == 0) {
+		cmd->background = true;
+	}
+
 	return cmd;
 }
 
@@ -62,9 +85,9 @@ int main(int argc, char** argv) {
 
 		printf("cmd: %s\targs: ", cmd->cmd);
 		for (int i = 0; i < cmd->argc; i++) {
-			printf("%s ", cmd->argv[i]);
+			printf("%s, ", cmd->argv[i]);
 		}
-		printf("\n");
+		printf("\ni_file: %s\to_file: %s\tbackground: %d\n", cmd->i_file, cmd->o_file, cmd->background);
 
 		if (strcmp(cmd->cmd, "exit") == 0) {
 			break;
